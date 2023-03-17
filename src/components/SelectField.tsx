164765e6a0d0328e6b34ddba2ecdd85803a888ad
Thomas Scelsi <tscelsi@evenergi.com>
2023-03-17 14:11:07 +0100

    feat: create SelectField

---
+++ src/components/SelectField.tsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import Select, { type OptionProps } from 'react-select'
+import classNames from 'classnames'
+
+type Props = {
+  label?: string
+  options: OptionProps[]
+}
+
+const SelectField = ({ options, label }: Props) => {
+  return (
+    <div className="flex flex-col gap-2">
+      {label && <span className="text-xs font-medium pl-2">{label}</span>}
+      <Select
+        unstyled
+        placeholder="Select an option"
+        options={options}
+        // override option display style
+        styles={{
+          option: () => ({
+            display: 'flex',
+          }),
+          noOptionsMessage: () => ({
+            display: 'flex'
+          }),
+        }}
+        classNames={{
+          control: (state) => classNames("hover:cursor-pointer bg-white px-3 border-black border-4 rounded-lg h-12 transition-all", {
+            "hover:shadow-blak": !state.menuIsOpen,
+            "hover:shadow-none": state.menuIsOpen,
+            "rounded-b-none": state.menuIsOpen,
+          }),
+          option: () => "bg-white flex pl-4 justify-start items-center h-12 border-t-0 border-b-2 last:border-b-4 border-black border-4 last:rounded-b-lg hover:bg-gray-100 hover:cursor-pointer",
+          placeholder: () => "text-gray-400 font-medium",
+          noOptionsMessage: () => "bg-gray-300 flex pl-4 justify-start items-center h-12 border-t-0 text-gray-400 font-medium border-black border-4 rounded-b-lg",
+        }}
+      />
+    </div>
+  )
+}
+
+export default SelectField
--- src/pages/_feed/manage.tsx
+++ src/pages/_feed/manage.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import classNames from 'classnames';
 import Navbar from '~/components/Navbar';
 import TextField from '~/components/TextField';
-import Select from 'react-select'
+import SelectField from '~/components/SelectField';
 
 
 export type LabelType = "SAME_EVENT" | "SAME_STORY" | "SAME_TOPIC" | "DIFFERENT";
@@ -21,29 +21,7 @@ const Manage: NextPage = () => {
 						<TextField bolden label="Feed name" />
 						<div className="mt-12 flex flex-col gap-4">
 							<h4 className="font-black text-xl">Outlet 1</h4>
-							<div className="flex flex-col gap-2">
-								<span className="text-xs font-medium pl-2">Outlet</span>
-								<Select
-									unstyled
-									placeholder="Select an option"
-									options={[{ label: "One", value: "one" }, { label: "Two", value: "two" }]}
-									// override option display style
-									styles={{
-										option: () => ({
-											display: 'flex',
-										})
-									}}
-									classNames={{
-										control: (state) => classNames("hover:cursor-pointer bg-white px-3 border-black border-4 rounded-lg h-12 transition-all", {
-											"hover:shadow-blak": !state.menuIsOpen,
-											"hover:shadow-none": state.menuIsOpen,
-											"rounded-b-none": state.menuIsOpen,
-										}),
-										option: () => "bg-white flex pl-4 justify-start items-center h-12 border-t-0 border-b-2 last:border-b-4 border-black border-4 last:rounded-b-lg hover:bg-gray-100 hover:cursor-pointer",
-										placeholder: () => "text-gray-400 font-medium",
-									}}
-								/>
-							</div>
+							<SelectField options={[]} label="Outlet" />
 							<TextField label="Endpoint" />
 						</div>
 					</div>
