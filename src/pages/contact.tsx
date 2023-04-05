5af366fdb7ed7b7875a7fe086cce884acd8ebdc2
Thomas Scelsi <tscelsi@evenergi.com>
2023-04-05 20:28:14 +0200

    feat: udpate contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -17,16 +17,16 @@ const Contact = () => {
         <Navbar type='sm'>Contact</Navbar>
       }
       <div className="flex flex-col mt-6 justify-center items-center font-medium gap-4">
-        <div className="min-w-[150px] flex gap-2 items-center border-black border-4 p-2 rounded-xl hover:shadow-blak hover:cursor-pointer transition-all">
-          <HiOutlineMail size={48} />
+        <div className="min-w-[150px] flex gap-2 items-center border-black border-4 p-2 rounded-lg hover:shadow-blak hover:cursor-pointer transition-all">
+          {/* <HiOutlineMail size={48} /> */}
           <span>Email</span>
         </div>
-        <div className="min-w-[150px] flex gap-2 items-center border-black border-4 p-2 rounded-xl hover:shadow-blak hover:cursor-pointer transition-all">
-          <Image src="/linkedin_small.svg" alt="linkedin logo" height={48} width={48} />
+        <div className="min-w-[150px] flex gap-2 items-center border-black border-4 p-2 rounded-lg hover:shadow-blak hover:cursor-pointer transition-all">
+          {/* <Image src="/linkedin_small.svg" alt="linkedin logo" height={48} width={48} /> */}
           <span>LinkedIn</span>
         </div>
-        <div className="min-w-[150px] flex gap-2 items-center border-black border-4 p-2 rounded-xl hover:shadow-blak hover:cursor-pointer transition-all">
-          <Image src="/github-mark.svg" alt="linkedin logo" height={48} width={48} />
+        <div className="min-w-[150px] flex gap-2 items-center border-black border-4 p-2 rounded-lg hover:shadow-blak hover:cursor-pointer transition-all">
+          {/* <Image src="/github-mark.svg" alt="linkedin logo" height={48} width={48} /> */}
           <span>Github</span>
         </div>
       </div>
